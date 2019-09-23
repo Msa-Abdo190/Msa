cbc99e63a1a970fef1ae8ddf99127f098c3c5ec7
Abdo190 <54249366+Msa-Abdo190@users.noreply.github.com>
2019-09-23 21:27:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== "207930773095972864") return;
+  if (message.author.id !== "ID حقك") return;
 
   
   if (message.content.startsWith(prefix + 'setwatch')) {
